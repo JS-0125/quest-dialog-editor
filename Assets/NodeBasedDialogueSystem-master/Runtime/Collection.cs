23216c7c576ebb052ebfe138f4212700d50c5781
Jung Jin Sun <realline@kpu.ac.kr>
2021-02-04 01:51:48 +0900

    collection 오브젝트에 닿았을 때 모으기 ui 띄우기
    collection 오브젝트에 닿았을 때 모으기 ui 띄우기

--- Assets/NodeBasedDialogueSystem-master/Runtime/Collection.cs
+++ Assets/NodeBasedDialogueSystem-master/Runtime/Collection.cs
@@ -2,24 +2,34 @@ using Subtegral.DialogueSystem.Runtime;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Collection : MonoBehaviour
 {
+    private GameObject collectionUi;
     public string guid;
-
+    private Vector3 position;
     private void Start()
     {
+        var Canvas = GameObject.Find("Canvas (2)");
+        collectionUi = Canvas.transform.Find("CollectionUi").gameObject;
         this.gameObject.SetActive(false);
+        position = this.transform.position;
+        position.y += 2;
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
-        {
+        { 
+            collectionUi.transform.position = position;
+            collectionUi.transform.rotation = Camera.main.transform.rotation;
+            collectionUi.gameObject.SetActive(true);
             if (Input.GetButtonDown("LeftClick"))
             {
                 this.gameObject.SetActive(false);
+                collectionUi.gameObject.SetActive(false);
 
                 Debug.Log("collect");
 
@@ -27,5 +37,10 @@ public class Collection : MonoBehaviour
                 camera.GetComponent<QuestParser>().CheckCollected(guid, this.gameObject);
             }
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        collectionUi.gameObject.SetActive(false);
     }
 }
